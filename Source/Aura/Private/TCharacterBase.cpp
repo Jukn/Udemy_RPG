bf797a19c3389d0fd8ca73eee994c08628b47730
Jukn <audqkr1478@gmail.com>
2024-01-30 16:55:45 +0900

    2-6

--- Source/Aura/Private/TCharacterBase.cpp
+++ Source/Aura/Private/TCharacterBase.cpp
@@ -7,7 +7,7 @@
 ATCharacterBase::ATCharacterBase()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
@@ -17,18 +17,3 @@ void ATCharacterBase::BeginPlay()
 	Super::BeginPlay();
 	
 }
-
-// Called every frame
-void ATCharacterBase::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void ATCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
-
